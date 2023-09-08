8d2e44099f786e604e140b4aa900538556be16a9
sirius506775 <sirius506775@gmail.com>
2023-09-09 01:59:43 +0900

    feat : Build a Service Discovery Server

---
+++ eureka-client1/src/main/java/com/example/eurekaclient1/EurekaClient1Application.java
@@ -0,0 +1,16 @@
+package com.example.eurekaclient1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+
+@EnableDiscoveryClient //it's no longer required
+@SpringBootApplication
+public class EurekaClient1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaClient1Application.class, args);
+	}
+
+}
